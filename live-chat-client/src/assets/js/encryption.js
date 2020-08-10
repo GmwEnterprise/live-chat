5c4ef13e52be441bc7dd0046988f5db8115a8448
GmwEnterprise <909498022@qq.com>
2020-08-10 21:59:55 +0800

    todo 完善前后端aes加密

---
+++ live-chat-client/src/assets/js/encryption.js
@@ -0,0 +1,26 @@
+import CryptoJS from 'crypto-js'
+
+var key = CryptoJS.enc.Utf8.parse("cBssbHB3ZA==HKXT")
+/*加密*/
+function aesEncode(str) {
+  var encryptedData = CryptoJS.AES.encrypt(str, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
+  })
+  return encryptedData.toString()
+}
+
+/*解密*/
+function aesDecode(encryptedStr) {
+  var encryptedHexStr = CryptoJS.enc.Base64.parse(encryptedStr)
+  var encryptedBase64Str = CryptoJS.enc.Base64.stringify(encryptedHexStr)
+  var decryptedData = CryptoJS.AES.decrypt(encryptedBase64Str, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.Pkcs7
+  })
+  return decryptedData.toString(CryptoJS.enc.Utf8)
+}
+
+export default {
+  aesDecode, aesEncode
+}
