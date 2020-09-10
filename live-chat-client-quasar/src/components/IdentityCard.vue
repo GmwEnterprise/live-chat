36ae0226d2833cbfaac5a79ddf44de72a934a88d
Mrag <909498022@qq.com>
2020-09-10 23:36:33 +0800

    待提交  名片的完成

---
+++ live-chat-client-quasar/src/components/IdentityCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="identity-card flex-center" style="align-items: center;">
+    <div class="id-card-contentarea">
+      <div class="head-area flex-between">
+        <div class="name-and-id">
+          <div>{{ name }}</div>
+          <div>{{ `微信号：${id}` }}</div>
+        </div>
+        <div class="avatar-box">
+          <img :src="avatar" v-if="avatar" />
+          <img src="~assets/avatar-default.png" v-else />
+        </div>
+        <!-- TODO -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gender from "assets/js/constants/gender";
+
+const type = {
+  me: 1,
+  friend: 2,
+  stranger: 3
+};
+
+export default {
+  name: "IdentityCard",
+  data() {
+    return {
+      id: "2312312398",
+      name: "阿甘",
+      avatar: "",
+      gender: gender.MALE,
+      type: type.friend,
+      backup: "学习哥",
+      location: "重庆",
+      origin: "通过群聊添加"
+    };
+  }
+};
+</script>
--- live-chat-client-quasar/src/components/friends/ChatGroup.vue
+++ live-chat-client-quasar/src/components/friends/ChatGroup.vue
@@ -1,14 +1,126 @@
 <template>
-  <div>群聊, id:{{ groupId }}</div>
+  <div class="chat-group-list" style="height: 100%; width: 100%;">
+    <div class="group-header">
+      {{ `${group.groupName}（${group.members.length}）` }}
+    </div>
+    <q-scroll-area
+      visible
+      style="height: calc(100% - 60px - 100px); width: 100%;"
+    >
+      <div ref="groupMembersArea" class="group-members-area">
+        <div
+          v-for="member in group.members"
+          :key="member.id"
+          class="group-member"
+        >
+          <div class="avatar-box">
+            <q-avatar square size="50px">
+              <img v-if="member.avatar" :src="member.avatar" />
+              <img v-else src="~assets/taylorswift.png" />
+            </q-avatar>
+          </div>
+          <div class="group-member-name">{{ member.name }}</div>
+        </div>
+      </div>
+    </q-scroll-area>
+    <div class="button-area flex-center" style="height: 100px;">
+      <q-btn
+        style="background: rgb(26 173 25 / 1); color: white; width: 120px;"
+        label="发消息"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+const group = {
+  groupName: "博文视点Java架构成长丛书读者群",
+  // groupMemberCount: 145,
+  members: [
+    {
+      id: "1",
+      name: "阿甘",
+      avatar: ""
+    },
+    {
+      id: "2",
+      name: "阿甘",
+      avatar: ""
+    },
+    {
+      id: "3",
+      name: "阿甘",
+      avatar: ""
+    }
+  ]
+};
+
 export default {
   name: "ChatGroup",
+  data() {
+    return {
+      group
+    };
+  },
   computed: {
     groupId() {
       return this.$route.params.groupId;
     }
+  },
+  created() {
+    for (let i = 5; i < 15; i++) {
+      this.group.members.push({
+        id: `${i}`,
+        name: "阿甘",
+        avatar: ""
+      });
+    }
   }
 };
 </script>
+
+<style scoped>
+.group-header {
+  border-bottom: 1px solid lightgray;
+  font-size: 20px;
+  line-height: 2.5;
+  padding: 0 20px;
+  padding-top: 10px;
+  box-sizing: border-box;
+  height: 60px;
+}
+.group-members-area {
+  display: flex;
+  justify-content: flex-start;
+  padding: 20px 50px 0;
+  flex-wrap: wrap;
+}
+.group-member {
+  width: 75px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.group-member:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.group-member-name {
+  color: gray;
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  line-height: 1.5;
+  font-size: 13px;
+  padding-bottom: 10px;
+}
+.avatar-box {
+  width: 75px;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
