f4016386ed9a1874ebead7427da6397a60a12be6
GmwEnterprise <909498022@qq.com>
2020-10-16 18:07:09 +0800

    gmw

---
+++ live-chat-stand-alone/src/main/java/com/github/mrag/livechat/LiveChatStandAloneApplication.java
@@ -0,0 +1,13 @@
+package com.github.mrag.livechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveChatStandAloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveChatStandAloneApplication.class, args);
+	}
+
+}
---
+++ live-chat-stand-alone/src/test/java/com/github/mrag/livechat/LiveChatStandAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrag.livechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveChatStandAloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
