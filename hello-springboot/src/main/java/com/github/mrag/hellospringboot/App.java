fa727789e3680173bd5dce43569b4204e724a72a
GmwEnterprise <909498022@qq.com>
2020-11-09 13:43:47 +0800

    gmw

--- hello-springboot/src/main/java/com/github/mrag/hellospringboot/App.java
+++ hello-springboot/src/main/java/com/github/mrag/hellospringboot/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        System.out.println(1);
         SpringApplication.run(App.class, args);
     }
 }
