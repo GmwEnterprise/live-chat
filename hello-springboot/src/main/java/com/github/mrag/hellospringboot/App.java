faa91e71d6ce5b1da6790ca32d3fec097c081957
GmwEnterprise <909498022@qq.com>
2020-11-11 17:31:58 +0800

    Gmw

--- hello-springboot/src/main/java/com/github/mrag/hellospringboot/App.java
+++ hello-springboot/src/main/java/com/github/mrag/hellospringboot/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println(1);
         SpringApplication.run(App.class, args);
     }
 }
--- hello-springframework/src/main/java/com/github/mrag/demo/spring/App.java
+++ hello-springframework/src/main/java/com/github/mrag/demo/spring/App.java
@@ -1,7 +1,14 @@
 package com.github.mrag.demo.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext()) {
+            ctx.register(AppConfig1.class, AppConfig2.class);
+            ctx.refresh();
 
+            Object bean = ctx.getBean("square");
+        }
     }
 }
